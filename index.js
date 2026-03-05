7e1d169a8c69de7d6aee76948bd611f0a1044d8e
Kyre0n <Samu21578@gmail.com>
2026-03-05 19:46:26 +0100

    Ejerecicio 3.5 hecho

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
-//app.use(express.json())
+app.use(express.json())
 
 let persons = [
     { 
@@ -45,9 +45,34 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-  
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    "name": body.name,
+    "number": body.number,
+    "id": Math.random() * 1000000
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
